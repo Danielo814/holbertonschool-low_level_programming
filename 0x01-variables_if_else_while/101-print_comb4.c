16d353ec7c70f35eb3e9ea4a1d7cc4098cf77142
Daniel Ojeda <211@holbertonschool.com>
2017-09-15 17:40:55 +0000

    three digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of three digits
+ *
+ * Description: 3 while loops to get 3 numbers with putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 48;
+	b = 49;
+	c = 50;
+	while (a < 58)
+	{
+		while (b < 58)
+		{
+			while (c < 58)
+			{
+				if (a != b && b != c && a != c && a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 | b != 56 | c != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			c = 48;
+			b++;
+		}
+		b = 48;
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
