e03bc3279ba02d6033fb432a2ccf1d2fb598931f
Daniel Ojeda <211@holbertonschool.com>
2017-09-19 00:58:05 +0000

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to check and print sign for
+ * Description: uses if statements to check if
+ * number is 0, greater than 0, or less than 0
+ * Return: 1 for positive, - for negative, 0 for 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar (45);
+		return (-1);
+	}
+	else
+	{
+		_putchar (48);
+		return (0);
+	}
+	return (0);
+}
