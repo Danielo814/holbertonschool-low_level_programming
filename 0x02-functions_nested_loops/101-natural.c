c2d2f44bbd3d34540b0d0de5e6475794294f8bbf
Daniel Ojeda <211@holbertonschool.com>
2017-09-20 21:36:59 +0000

    changed variable names

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,16 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+	int a, b, c;
+	int sum;
 
 	for (a = 0; a < 1024; a++)
 	{
 		b = a % 3;
 		c = a % 5;
 		if (b == 0 || c == 0)
-			d = d + a;
+			sum = sum + a;
 	}
-	printf("%d\n", d);
+	printf("%d\n", sum);
 	return (0);
 }
