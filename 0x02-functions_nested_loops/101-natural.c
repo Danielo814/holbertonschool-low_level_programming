22f3b2d3fe9ee2e16ac5f51d7bed00578f31ce45
Daniel Ojeda <211@holbertonschool.com>
2017-09-20 21:26:40 +0000

    print sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints sum of all natural numbers from 0 - 1024
+ *
+ * Description: uses for loop to pass through all characters
+ * and find which ones are divisible by 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 1024; a++)
+	{
+		b = a % 3;
+		c = a % 5;
+		if (b == 0 || c == 0)
+		{
+			d = d + a;
+		}
+	}
+	printf("%d\n", d);
+	return (0);
+}
