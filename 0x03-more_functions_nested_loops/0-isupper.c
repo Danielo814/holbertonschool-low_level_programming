bd79b7419943176044def1b0036539b006974454
Daniel Ojeda <211@holbertonschool.com>
2017-09-21 21:14:45 +0000

    checks if number is uppercase or lowercase

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ * Description: will check if number is less than Z or
+ * greater than A
+ * Return: 1 if uppercase 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
