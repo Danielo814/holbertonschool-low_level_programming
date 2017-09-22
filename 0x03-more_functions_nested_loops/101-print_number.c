aa757151b4525f7e1c3468e7aa28b4205f7bfe8d
Daniel Ojeda <211@holbertonschool.com>
2017-09-22 21:28:12 +0000

    removing standard library

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_number - prints an integer
  *
@@ -16,7 +15,7 @@ void print_number(int n)
 	}
 	if (n == 0)
 		_putchar(48);
-	if (n / 10 != '\0')
+	if (n / 10)
 		print_number(n / 10);
 	_putchar(n % 10 + 48);
 }
