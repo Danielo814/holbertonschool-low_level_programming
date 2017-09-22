da7fa10dc0068142d2b9bc31cc7ffbd896b988bc
Daniel Ojeda <211@holbertonschool.com>
2017-09-22 21:21:55 +0000

    recursion to print any integer

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - prints an integer
+ *
+ * @n: number to print
+ * Description: uses recursion to print an integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar(48);
+	if (n / 10 != '\0')
+		print_number(n / 10);
+	_putchar(n % 10 + 48);
+}
