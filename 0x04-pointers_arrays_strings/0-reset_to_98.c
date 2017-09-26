cd52275550e48025f2730207c67cd0611f459071
Daniel Ojeda <211@holbertonschool.com>
2017-09-26 19:32:23 +0000

    resets value of n

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - updates value of 9 in main
+ * @n: pointer value to dereference in main
+ * Description: dereferences n in main to update value
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
