df4875f98c1a51f6b9a986bcae7ce062f3374d87
Daniel Ojeda <211@holbertonschool.com>
2017-10-04 21:22:05 +0000

    prints string using recursion

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -7,13 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	while (s[i])
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(10);
+		return;
 	}
-	_putchar(10);
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
