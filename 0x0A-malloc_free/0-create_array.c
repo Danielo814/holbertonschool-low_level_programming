0bb96957051766050beab1a6acf8c3ab01c6b743
Daniel Ojeda <211@holbertonschool.com>
2017-10-10 20:24:44 +0000

    create array of chars

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	n = malloc((size + 1) * sizeof(char));
-	if (n == 0)
+	if (size == 0)
 		return (NULL);
 	i = 0;
 	while (i < size)
