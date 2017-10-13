aab5dfd5fc1587280a4be88e94a7f3e52598db6f
Daniel Ojeda <211@holbertonschool.com>
2017-10-13 01:30:35 +0000

    allocates memory

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: size
+ *
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (p);
+}
