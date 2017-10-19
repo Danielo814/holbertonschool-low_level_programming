05843adfb0980cc489344284efdae4e93a6e4c9d
Daniel Ojeda <211@holbertonschool.com>
2017-10-19 06:01:47 +0000

    functions

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subrtacts numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides numbers
+ * @a: first number
+ * @b: second number
+ * Return: division of numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - divides numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
