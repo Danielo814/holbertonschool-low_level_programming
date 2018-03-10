e248f1cd644d6745b84107c02d9b515859b39dab
Daniel Ojeda <211@holbertonschool.com>
2018-03-10 01:22:52 +0000

    inserts left

---
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts a node as left child
+ * @parent: pointer to parent
+ * @value: value of new
+ *
+ * Return: pointer to new
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	if (parent->left)
+	{
+		new->left = parent->left;
+		parent->left = new;
+		new->left->parent = new;
+	}
+	parent->left = new;
+	return (new);
+}
