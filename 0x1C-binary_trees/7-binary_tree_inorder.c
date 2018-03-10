3d030f0f05c6e100d89d1ec867347d7967584cdd
Daniel Ojeda <211@holbertonschool.com>
2018-03-10 01:58:29 +0000

    binary tree in order pring

---
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - uses in  order traversal
+ * @tree: binary tree to traverse
+ * @func: function used to print
+ *
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!func)
+		return;
+	if (!tree)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n)
+	binary_tree_inorder(tree->right, func);
+}
