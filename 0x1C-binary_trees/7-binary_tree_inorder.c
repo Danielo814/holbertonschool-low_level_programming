5393409d0acbb5d2a1fce3e690de889e3ae970fc
Daniel Ojeda <211@holbertonschool.com>
2018-03-10 02:01:38 +0000

    missing semi colon

--- 0x1C-binary_trees/7-binary_tree_inorder.c
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -7,11 +7,11 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!func)
-		return;
 	if (!tree)
 		return;
+	if (!func)
+		return;
 	binary_tree_inorder(tree->left, func);
-	func(tree->n)
+	func(tree->n);
 	binary_tree_inorder(tree->right, func);
 }
