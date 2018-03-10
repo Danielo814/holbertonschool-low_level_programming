02360992c328c66d51af4abdb8a566cf004a777b
Daniel Ojeda <211@holbertonschool.com>
2018-03-10 05:21:05 +0000

    changes

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !node->parent || !node->parent->left || !node->parent->right)
+	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->left->n == node->n)
-		return (node->parent->right);
-	else
+	if (node->parent->right == node)
 		return (node->parent->left);
+	else
+		return (node->parent->right);
 }
