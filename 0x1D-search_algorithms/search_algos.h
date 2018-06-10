22b445dddd99910aeed352529175961bae6f15f1
Daniel Ojeda <211@holbertonschool.com>
2018-06-09 19:06:54 -0700

    Delete search_algos.h

--- 0x1D-search_algorithms/search_algos.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SEARCHALGOSH__
-#define __SEARCHALGOSH__
-#include <stdio.h>
-
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-
-#endif
