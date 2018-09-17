fe0e8e3870d681f52fe113f06814ad291291e8a3
Daniel Ojeda <211@holbertonschool.com>
2018-09-17 03:43:18 +0000

    set counter equal to 0

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *temp;
 	unsigned int i;
 
-	for (temp = head; i < index && temp != NULL; i++)
+	for (temp = head, i = 0; i < index && temp != NULL; i++)
 		temp = temp->next;
 
 	if (temp == NULL)
