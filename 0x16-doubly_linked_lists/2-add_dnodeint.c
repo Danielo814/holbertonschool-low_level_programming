df63331f65cab13faeb800c00581074c1ee4a2a5
Daniel Ojeda <211@holbertonschool.com>
2018-09-17 01:25:17 +0000

    adds a new node at the begging of list

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -17,9 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (head != NULL)
 	{
+		new_node->next = *head;
 		new_node->n = n;
 		new_node->prev = NULL;
-		new_node->next = *head;
+		if (*head)
+			(*head)->prev = new_node;
 		*head = new_node;
 	}
 	return (new_node);
