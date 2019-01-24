fc2fdde1b4f40693c14663faabfa4526f4e2d79d
Daniel Ojeda <211@holbertonschool.com>
2019-01-24 01:44:54 +0000

    header file

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef __searchalgosh__
+#define __searchalgosh__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
